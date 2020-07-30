a015b9d81ed7ae21093c4fcf8c77978270d76716
Alan McCosh <alan@while.software>
2020-07-29 21:10:56 -0700

    switching to specific patched version for my SwiftyJSON

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
-        .package(url: "https://github.com/whilesoftware/SwiftyJSON.git", .revision("7080c04af17ca65802a2c4511f64fe12f851ed61"))
+        .package(url: "https://github.com/whilesoftware/SwiftyJSON.git", from: "5.0.0-patched")
     ],
     targets: [
         .target(
